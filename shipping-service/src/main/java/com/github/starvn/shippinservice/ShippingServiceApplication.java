debdf88e7d3c66790c6ce6c199b465e3d4037852
Huy Duc Dao <ducdh.starvn@gmail.com>
2023-12-30 18:38:00 +0700

    Refactoring code

--- shipping-service/src/main/java/com/github/starvn/shippinservice/ShippingServiceApplication.java
+++ shipping-service/src/main/java/com/github/starvn/shippinservice/ShippingServiceApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ShippinServiceApplication {
+public class ShippingServiceApplication {
 
   public static void main(String[] args) {
-    SpringApplication.run(ShippinServiceApplication.class, args);
+    SpringApplication.run(ShippingServiceApplication.class, args);
   }
 
 }
