34b47a19f63155e2005340860000217d7c49fcd1
Huy Duc Dao <ducdh.starvn@gmail.com>
2023-12-30 18:36:12 +0700

    Update project structure

---
+++ inventory-service/src/main/java/com/github/starvn/inventoryservice/InventoryServiceApplication.java
@@ -0,0 +1,13 @@
+package com.github.starvn.inventoryservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InventoryServiceApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(InventoryServiceApplication.class, args);
+  }
+
+}
---
+++ shipping-service/src/main/java/com/github/starvn/shippinservice/ShippinServiceApplication.java
@@ -0,0 +1,13 @@
+package com.github.starvn.shippinservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShippinServiceApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(ShippinServiceApplication.class, args);
+  }
+
+}
